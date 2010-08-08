9554beeb73ac04807949cbd8a8c853f7bdc45789
Alex Poga <alex.poga@gmail.com>
2010-08-08 12:55:37 +0800

    Added basic GUI for main frame

---
+++ PatientWaitingList/src/patientwaitinglist/PatientWaitingListApp.java
@@ -0,0 +1,44 @@
+/*
+ * PatientWaitingListApp.java
+ */
+
+package patientwaitinglist;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class PatientWaitingListApp extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new PatientWaitingListView(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of PatientWaitingListApp
+     */
+    public static PatientWaitingListApp getApplication() {
+        return Application.getInstance(PatientWaitingListApp.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(PatientWaitingListApp.class, args);
+    }
+}
